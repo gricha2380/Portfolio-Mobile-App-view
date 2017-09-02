7486d3ed10f45834ca9467cd878491ba76d0b18d
Gregor Richardson <gregor.com@mac.com>
2017-09-02 14:13:08 -0700

    multisheet is actualy working (somehow)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,20 +3,33 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
       function init() {
         Tabletop.init( { key: public_spreadsheet_url,
                          callback: showInfo,
-                         simpleSheet: true } );
+                         simpleSheet: false,
+                         wanted: ["Portfolio"] } );
       }
 
       window.addEventListener('DOMContentLoaded', init)
 
-      function showInfo(data) {
+      function showInfo(data, tabletop) {
         // data comes through as a simple array since simpleSheet is turned on
         //document.getElementById("data").innerHTML = JSON.stringify(data);
 
         //holder = data;
+        //console.log(tabletop.sheets("Portfolio")["columnNames"]);
+        var holder = tabletop.sheets("Portfolio");
         var before = '<td>', after = '</td>', row='', beforeRow = '<tr>', afterRow = '</tr>';
         var table = '<table class="sheetTable ui celled unstackable table">';
+        var tableComplex = '<table class="sheetTable ui celled unstackable table GG">';
+        var maxRows = 10
+
+        // table header simpleSheet false
+        tableComplex += '<thead><tr>';
+        for (var i = 0;i < maxRows;i++) { // or use holder["columnNames"].length
+          tableComplex += "<th>"+holder["columnNames"][i]+"</th>";
+        }
+        tableComplex += "</tr></thead>";
 
         // table header
+        /*
         var e = 0;
         table += '<thead><tr>';
         for (var h in data[0]) {
@@ -26,6 +39,8 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
           }
         }
         table += "</tr></thead>";
+        */
+
 
         //duplicate header
         /*row += beforeRow;
@@ -40,6 +55,27 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
         row += afterRow;
         */
 
+        console.log(holder["elements"][2]);
+        // row complex
+        tableComplex += beforeRow;
+        for (var i = 0; i < holder["elements"].length; i++) {
+          var rowCounter = 1; // loop for each cell of data
+          /*
+          for (var r = 0; r < holder["elements"][i].length; r++) {
+            tableComplex += before + holder["elements"][i][r] + after;
+            console.log(holder["elements"][i][r]);
+          }
+          */
+          for (var e in holder["elements"][i]) {
+            if (rowCounter < maxRows) {
+              tableComplex += before + holder["elements"][i][e] + after;
+              rowCounter++;
+            }
+          }
+          tableComplex += before + holder["elements"][i]["Gain 24h"]; // manually add last row
+          tableComplex += afterRow;
+        }
+        /* row simple
         for (var i = 0; i < data.length; i++) { // each row of spreadsheet data
           row += beforeRow;
           var rowCounter = 1; // loop for each cell of data
@@ -53,9 +89,12 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
           row += afterRow;
 
         };
-        document.getElementById("portfolio").innerHTML = table + row + "</table>" ;
+        */
+
+        //document.getElementById("portfolio").innerHTML = table + row + "</table>" ;
         // final output
         //document.getElementById("data").innerHTML = data[0]+ /*x +" : "+ */ table + row + "</table>";
+        document.getElementById("portfolio").innerHTML = tableComplex + "</table>" ;
 
       }
 
@@ -64,4 +103,12 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
 
  $('.menu .item').tab();
 
- document.getElementById("stats")
+document.getElementById("stats").addEventListener("click", function(){
+
+        Tabletop.init( { key: public_spreadsheet_url,
+                         callback: showInfo,
+                         simpleSheet: true } );
+        function showStatsInfo(data) {
+
+        }
+});
