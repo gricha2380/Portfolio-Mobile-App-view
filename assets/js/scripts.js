8a644e28dcee258efc133ad077e4b70d263a06be
GRicha2380 <gregor.richardson@gmail.com>
2017-09-24 12:42:58 -0700

    conditional formatting on stats & interactive chart

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1KrKXcbFDnM
 var urlStocks = 'https://spreadsheets.google.com/feeds/list/1KrKXcbFDnMjLGoM4WdKz24M4kAz2wERqYHVKFPdHm_U/1/public/values?alt=json';
 var urlStats = 'https://spreadsheets.google.com/feeds/list/1KrKXcbFDnMjLGoM4WdKz24M4kAz2wERqYHVKFPdHm_U/2/public/values?alt=json';
 var urlChart = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive',
-  chartHTML;
+    chartHTML;
 var before = '<td>',
     after = '</td>',
     row = '',
@@ -16,9 +16,12 @@ var beforeNeg = '<td class="neg">',
     beforeBold = "<td class='bold'>";
 var table = '<table class="sheetTable ui celled unstackable table GG" data-sortlist="[[5,1]]">';
 var statRow = '<div class="row">',
+    statRowLower = '<div class="row rowLower">',
     endDiv = '</div>',
     statLabel = '<div class="label">',
     statValue = '<div class="value">',
+    statValuePos = '<div class="value pos">',
+    statValueNeg = '<div class="value neg">',
     statValueMain = '<div class="valueMain">',
     statContainer = '<div class="container">';
 
@@ -49,31 +52,46 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
     function showChart() {
-      //document.querySelector("#chartHolder").innerHTML ='<iframe width="897" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>'
-      //document.getElementById("stats").innerHTML =
-      //document.querySelector("#stats").innerHTML += '<iframe height="371" id="statsChart" seamless frameborder="0" allowtransparency="true" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>';
-      document.querySelector("#stats").innerHTML += '<div class="iframeHolder"><iframe width="'+"897"+'" height="390" id="statsChart" seamless frameborder="0" allowtransparency="true" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=image"></iframe></div>';
-      //$("#chartHolder").html('<iframe width="897" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>');
-      //document.body.offsetWidth
+        //document.querySelector("#chartHolder").innerHTML ='<iframe width="897" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>'
+        //document.getElementById("stats").innerHTML =
+        //document.querySelector("#stats").innerHTML += '<iframe height="371" id="statsChart" seamless frameborder="0" allowtransparency="true" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>';
+        document.querySelector("#stats").innerHTML += '<div class="iframeHolder"><iframe width="' + "897" + '" height="390" id="statsChart" seamless frameborder="0" allowtransparency="true" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe></div>';
+        //$("#chartHolder").html('<iframe width="897" height="371" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTb5CY1SKVQhJUOHkF1Dxoviu4dhhBbHCkMvmn-0pQ_VX14xEIHspTZHVSxxWIEcXubZQ6rDpinTDAV/pubchart?oid=2089747653&amp;format=interactive"></iframe>');
+        //document.body.offsetWidth
     }
 
     function showStats(data) {
-        console.log("full JSON... ",data);
+        console.log("full JSON... ", data);
         var list = data["feed"]["entry"]["0"];
         console.log("list contents", list);
 
-        //console.log(statRow + statLabel + "Portfolio" + endDiv + statValueMain + list["gsx$portfoliovalue"]["$t"] + endDiv);
         document.getElementById("stats").innerHTML =
-            statRow + statLabel + "Portfolio" + endDiv + statValueMain + list["gsx$portfoliovalue"]["$t"] + endDiv + endDiv +
-            statRow + statContainer + statLabel + "Portfolio Gains" + endDiv + statValue + list["gsx$portfoliogains"]["$t"] + endDiv + endDiv +
-            statContainer + statLabel + "Portfolio Growth" + endDiv + statValue + list["gsx$portfoliogrowth"]["$t"] + endDiv + endDiv
-            //+statRow+statContainer+statLabel+"Crypto Gains"+endDiv+statValue+list["gsx$cryptogains"]["$t"]+endDiv+endDiv
-            //+statRow+statContainer+statLabel+"Crypto Growth"+endDiv+statValue+list["gsx$cryptogrowth"]["$t"]+endDiv+endDiv
-            // why can I only see the first row of the JSON?
-            +
-            "<div class='viewSpreadsheet'><a class='bottomLink'target='_new' href='" + public_spreadsheet_url + "'>" + "View Spreadsheet" + "</a></div>"+"";
+            statRow + statLabel + "Portfolio" + endDiv + statValueMain + list["gsx$portfoliovalue"]["$t"] + endDiv + endDiv;
 
+        //conditional formatting for portfolio gains $
+        if (parseFloat(Number(list["gsx$portfoliogains"]["$t"].replace(/[^0-9-\.]+/g, ""))) > 0) {
+            document.getElementById("stats").innerHTML += statRowLower + statContainer + statLabel + "Portfolio Gains" + endDiv + statValuePos + list["gsx$portfoliogains"]["$t"] + endDiv + endDiv;
+        } else if (parseFloat(Number(list["gsx$portfoliogains"]["$t"].replace(/[^0-9-\.]+/g, ""))) == 0) {
+            document.getElementById("stats").innerHTML += statRowLower + statContainer + statLabel + "Portfolio Gains" + endDiv + statValue + list["gsx$portfoliogains"]["$t"] + endDiv + endDiv;
+        } else {
+            document.getElementById("stats").innerHTML += statRowLower + statContainer + statLabel + "Portfolio Gains" + endDiv + statValueNeg + list["gsx$portfoliogains"]["$t"] + endDiv + endDiv;
+        }
+
+        //conditional formatting for portfolio growth %
+        if (parseFloat(Number(list["gsx$portfoliogrowth"]["$t"])) > 0) {
+            document.querySelector("#stats .rowLower").innerHTML += statContainer + statLabel + "Portfolio Growth" + endDiv + statValuePos + list["gsx$portfoliogrowth"]["$t"] + endDiv + endDiv;
+        } else if (parseFloat(Number(list["gsx$portfoliogrowth"]["$t"])) == 0) {
+            ddocument.querySelector("#stats .rowLower").innerHTML += statContainer + statLabel + "Portfolio Growth" + endDiv + statValue + list["gsx$portfoliogrowth"]["$t"] + endDiv + endDiv;
+        } else {
+            document.querySelector("#stats .rowLower").innerHTML += statContainer + statLabel + "Portfolio Growth" + endDiv + statValueNeg + list["gsx$portfoliogrowth"]["$t"] + endDiv + endDiv;
+        }
+
+        // why can I only see the first row of the JSON?
+
+        document.getElementById("stats").innerHTML += "<div class='viewSpreadsheet'><a class='bottomLink'target='_new' href='" + public_spreadsheet_url + "'>" + "View Spreadsheet" + "</a></div>" + "";
     }
+
+
 
 
     document.querySelector("#portfolioTab").addEventListener("click", function() {
@@ -120,7 +138,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 table += beforeNeg + data["feed"]["entry"][i][colname]["$t"] + after;
             }
 
-            colname = "gsx$totalgain"; // total gain $
+            colname = "gsx$totalgain"; // total gains $
             if (parseFloat(Number(data["feed"]["entry"][i][colname]["$t"].replace(/[^0-9-\.]+/g, ""))) > 0) {
                 table += beforePos + data["feed"]["entry"][i][colname]["$t"] + after;
             } else if (parseFloat(Number(data["feed"]["entry"][i][colname]["$t"].replace(/[^0-9-\.]+/g, ""))) == 0 || parseFloat(data["feed"]["entry"][i][colname]["$t"]) == "-") {
